30725107daca15e6ec20ce0e08373eaaca0c914f
Isaac Ellwood <kingisaacellwood@gmail.com>
2022-06-09 12:32:21 +1200

    Added files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="logo box"></div><!--end logo-->
+            <div class="banner box">
+                Isaac's Movie Review Website
+            </div><!--end banner-->
+            <div class="left-side box"></div><!--end left side-->
+            <div class="main box"></div><!--end main-->
+            <div class="footer box"></div><!--end footer-->
+        </div><!--end wrapper-->
+    </body>
+</html>
