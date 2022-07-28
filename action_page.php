454d4adea9c57b433719788545744b65a3d046f5
Isaac Ellwood <kingisaacellwood@gmail.com>
2022-07-28 13:36:33 +1200

    Changed colour scheme/added forms

---
+++ action_page.php
@@ -0,0 +1,13 @@
+<?php include("top_bit.html");?>
+            <div class="main box">
+                <div class="main-head box">
+                    <h1>Response title</h1>
+                </div><!--end main-head-->
+                <div class="main-left box">
+                    Thank you for submitting a response. Thsi currently doesn't work tho lol.
+                </div><!--end main-left-->
+                <div class="main-right box">
+                    naimn right
+                </div><!--end main-right-->
+            </div><!--end main-->
+<?php include("bottom_bit.html");?>
---
+++ contact_me.php
@@ -0,0 +1,19 @@
+<?php include("top_bit.html");?>
+            <div class="main box">
+                <div class="main-head box">
+                    <h1>Contact Me</h1>
+                </div><!--end main-head-->
+                <div class="main-left box">
+                    <form action="action_page.php" method="post">
+                        <label for="fname">First name:</label>
+                        <input type="text" id="fname" name="fname"><br><br>
+                        <label for="lname">Last name:</label>
+                        <input type="text" id="lname" name="lname"><br><br>
+                        <input type="submit" value="Submit">
+                    </form>
+                </div><!--end main-left-->
+                <div class="main-right box">
+                    naimn right
+                </div><!--end main-right-->
+            </div><!--end main-->
+<?php include("bottom_bit.html");?>
