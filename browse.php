5ffc985d3808a8354f09d41b01c67a653d374492
Isaac Ellwood <92236998+Isaac-Ellwood@users.noreply.github.com>
2022-08-19 07:19:35 +1200

    Final bug fixes

--- browse.php
+++ browse.php
@@ -6,9 +6,9 @@
                 <a href="Tenet.php"><div class="item"><img src="images/movie_posters/Tenet.jpg" alt="Tenet"><br>Tenet<br>Christopher Nolan<br>2020</div></a>
                 <a href="Edward_Scissorhands.php"><div class="item"><img src="images/movie_posters/EdwardScissorhands.jpg" alt="Edward Scissorhands"><br>Edward Scissorhands<br>Tim Burton<br>1990</div></a>
                 <a href="Baby_Driver.php"><div class="item"><img src="images/movie_posters/BabyDriver.jpg" alt="Baby Driver"><br>Baby Driver<br>Edgar Wright<br>2017</div></a>
-                <a href="Don`t_Look_Up.php"><div class="item"><img src="images/movie_posters/Don`tLookUp.jpg" alt="Don`t Look Up"><br>Don`t Look Up<br>Adam McKay<br>2021</div></a>
+                <a href="Don't_Look_Up.php"><div class="item"><img src="images/movie_posters/Don`tLookUp.jpg" alt="Don`t Look Up"><br>Don`t Look Up<br>Adam McKay<br>2021</div></a>
                 <a href="Lady_Bird.php"><div class="item"><img src="images/movie_posters/LadyBird.jpg" alt="Lady Bird"><br>Lady Bird<br>Greta Gerwig<br>2017</div></a>
-                <a href="Ocean`s_Eleven.php"><div class="item"><img src="images/movie_posters/Ocean`sEleven.jpg" alt="Ocean`s Eleven"><br>Ocean`s Eleven<br>Steven Soderbergh<br>2001</div></a>
+                <a href="Ocean's_Eleven.php"><div class="item"><img src="images/movie_posters/Ocean`sEleven.jpg" alt="Ocean`s Eleven"><br>Ocean`s Eleven<br>Steven Soderbergh<br>2001</div></a>
                 <a href="Inception.php"><div class="item"><img src="images/movie_posters/Inception.jpg" alt="Inception"><br>Inception<br>Christopher Nolan<br>2010</div></a>
                 <a href="Interstellar.php"><div class="item"><img src="images/movie_posters/Interstellar.jpg" alt="Interstellar"><br>Interstellar<br>Christopher Nolan<br>2014</div></a>
                 <a href="Hunt_for_the_Wilderpeople.php"><div class="item"><img src="images/movie_posters/HuntfortheWilderpeople.jpg" alt="Hunt for the Wilderpeople"><br>Hunt for the Wilderpeople<br>Taika Waititi<br>2016</div></a>
--- fair_use.php
+++ fair_use.php
@@ -6,22 +6,21 @@
                 <div class="main-left box">
                     All images used are used under fair use for educational purposes.
                     <br>Movie Images can be found at:
-                    <br><a href="https://www.imdb.com/title/tt0499549/">link</a>
-                    <br><a href="https://www.imdb.com/title/tt8171236/">link</a>
-                    <br><a href="https://www.imdb.com/title/tt1192628/">link</a>
-                    <br><a href="https://en.wikipedia.org/wiki/Tenet_%28film%29">link</a>
-                    <br><a href="https://www.20thcenturystudios.com.au/movies/edward-scissorhands">link</a>
-                    <br><a href="https://www.imdb.com/title/tt3890160/">link</a>
-                    <br><a href="https://www.imdb.com/title/tt11286314/">link</a>
-                    <br><a href="https://www.imdb.com/title/tt4925292/">link</a>
-                    <br><a href="https://www.justwatch.com/nz/movie/oceans-eleven">link</a>
-                    <br><a href="https://www.imdb.com/title/tt1375666/">link</a>
-                    <br><a href="https://www.imdb.com/title/tt0816692/">link</a>
-                    <br><a href="https://en.wikipedia.org/wiki/Hunt_for_the_Wilderpeople">link</a>
+                    <br><a href="https://www.imdb.com/title/tt0499549/">Avatar</a>
+                    <br><a href="https://www.imdb.com/title/tt8171236/">This Town</a>
+                    <br><a href="https://www.imdb.com/title/tt1192628/">Rango</a>
+                    <br><a href="https://en.wikipedia.org/wiki/Tenet_%28film%29">Tenet</a>
+                    <br><a href="https://www.20thcenturystudios.com.au/movies/edward-scissorhands">Edward Scissorhands</a>
+                    <br><a href="https://www.imdb.com/title/tt3890160/">Baby Driver</a>
+                    <br><a href="https://www.imdb.com/title/tt11286314/">Don't look up</a>
+                    <br><a href="https://www.imdb.com/title/tt4925292/">Lady Bird</a>
+                    <br><a href="https://www.justwatch.com/nz/movie/oceans-eleven">Ocean's Eleven</a>
+                    <br><a href="https://www.imdb.com/title/tt1375666/">Inception</a>
+                    <br><a href="https://www.imdb.com/title/tt0816692/">Interstellar</a>
+                    <br><a href="https://en.wikipedia.org/wiki/Hunt_for_the_Wilderpeople">Hunt for the Wilderpeople</a>
                     <br>Other images:
-                    <br><a href="https://unsplash.com/photos/vZJdYl5JVXY">link</a>
-                    <br><a href="https://unsplash.com/photos/wMkaMXTJjlQ">link</a>
-                    <br><a href="https://unsplash.com/photos/X_d7m2r70bA">link</a>
+                    <br><a href="https://unsplash.com/photos/wMkaMXTJjlQ">Background Image</a>
+                    <br><a href="https://unsplash.com/photos/X_d7m2r70bA">Logo</a>
                 </div><!--end main-left-->
                 <div class="main-right box">
                     naimn right
